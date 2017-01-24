175c79891619738fe4bf4e42af9dae78cce85f9a
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-01-24 12:52:32 +0300

    grid fix

--- lib/src/request_list_component.dart
+++ lib/src/request_list_component.dart
@@ -23,55 +23,23 @@ class RequestListComponent implements OnInit {
 
   @override
   void ngOnInit() {
-    var model = new DataSourceSchemaModelWithFieldsArray();
-    model.fields = new List<DataSourceSchemaModelField>();
 
-    model.fields.add(
-        new DataSourceSchemaModelField(field: "creationDate", type: "date"));
-    model.fields.add(
-        new DataSourceSchemaModelField(field: "correctionDate", type: "date"));
 
-    var schema = new DataSourceSchema()..model = model;
-
-    var transportRead = new DataSourceTransportRead()
-      ..type = "get"
-      ..dataType = "json"
-      ..url = "//cm-ylng-msk-01/cmas-backend/api/request/list";      
-
-    var transport = new DataSourceTransport()..read = transportRead;
-
-    var dataSource = new DataSource()
-      ..type = "odata"
-      ..schema = schema
-      ..transport = transport;
+    var columns = new List<Column>();
+    columns.add(new Column(field: 'id', caption: '№', size: '150px'));
+    columns.add(new Column(field: 'contractorName', caption: 'Подрядчик', size: '200px'));
+    columns.add(new Column(field: 'creationDate', caption: 'Дата создания', render: 'date', size: '100px'));
+    columns.add(new Column(field: 'correctionDate', caption: 'Дата изменения', render: 'date', size: '100px'));
+    columns.add(new Column(field: 'contractNumber', caption: '№ договора', size: '200px'));
+    columns.add(new Column(field: 'amount', caption: 'Сумма к оплате', size: '100px'));
+    columns.add(new Column(field: 'status', caption: 'Статус', size: '100px'));
 
     GridOptions options = new GridOptions()
-      ..dataSource = dataSource
-      ..columns = new List<GridColumn>()
-      ..filterable = true
-      //..height = 500
-      ..sortable = true;
+      ..name = 'grid'
+      ..columns = columns
+      ..url='//cm-ylng-msk-01/cmas-backend/api/request/list'
+      ..method='GET';
 
-    options.columns.add(
-        new GridColumn(field: "id", title: "№", width: 150, filterable: true));
-    options.columns.add(new GridColumn(
-        field: "contractorName", title: "Подрядчик", sortable: true));
-    options.columns.add(new GridColumn(
-        field: "creationDate",
-        title: "Дата создания",
-        sortable: true,
-        format: "{0: dd/MM/yyyy}"));
-    options.columns.add(new GridColumn(
-        field: "correctionDate",
-        title: "Дата изменения",
-        sortable: true,
-        format: "{0: dd/MM/yyyy}"));
-    options.columns.add(new GridColumn(
-        field: "contractNumber", title: "№ договора", sortable: true));
-    options.columns.add(new GridColumn(
-        field: "amount", title: "Сумма к оплате", sortable: true));
-    options.columns
-        .add(new GridColumn(field: "status", title: "Статус", sortable: true));
 
     new Grid(this._resourcesLoaderService, "#grid", options);
   }
