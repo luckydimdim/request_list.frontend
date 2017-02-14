6555080f7d78f19bb31a1647d5dfd1c3c2f6889d
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-02-14 16:11:44 +0300

    reorganization

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'dart:core';
 
-import 'dart:html';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
 import 'package:angular2/src/core/reflection/reflection.dart';
@@ -9,19 +7,27 @@ import 'package:angular2/router.dart';
 import 'package:angular2/src/platform/browser/location/hash_location_strategy.dart';
 import 'package:angular2/platform/common.dart';
 
-import 'package:request_list/request_list_component.dart';
+import 'package:master_layout/master_layout_component.dart';
 import 'package:resources_loader/resources_loader.dart';
+
+import 'package:request_list/request_list_component.dart';
 
 bool get isDebug =>
     (const String.fromEnvironment('PRODUCTION', defaultValue: 'false')) !=
     'true';
+
+@Component(selector: 'app')
+@View(
+  template: '<master-layout><request-list></request-list></master-layout>',
+  directives: const [MasterLayoutComponent, RequestListComponent])
+class AppComponent {}
 
 main() async {
   if (isDebug) {
     reflector.trackUsage();
   }
 
-  ComponentRef ref = await bootstrap(RequestListComponent, [
+  ComponentRef ref = await bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(ResourcesLoaderService),
